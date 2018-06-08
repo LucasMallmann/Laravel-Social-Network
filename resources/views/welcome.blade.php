36251fcce28fbc30a6d2511d6a346f9575951f0a
Lucas Mallmann <lucas.ribeiro@object1ve.com>
2018-06-08 13:40:52 -0300

    login view finished

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,45 @@
-@extends('layouts.master')
+<link rel="stylesheet" href="/public/css/welcome.css">
 
-@section('title')
-    Welcome
-@endsection
 
-@section('content')
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-					<form action="" method="POST">
-							<div class="form-group">
-									<label for="email">Email</label>
-									<input class="form-control" type="text" name="email" id="email">
-							</div>
-							<div class="form-group">
-									<label for="first_name">First Name</label>
-									<input class="form-control" type="text" name="first_name" id="first_name">
-							</div>
-							<div class="form-group">
-									<label for="password">Email</label>
-									<input class="form-control" type="password" name="password" id="password">
-							</div>
-							<button type="submit" class="btn btn-primary">Submit</button>
-					</form>
-			</div>
-		</div>		
+@extends('layouts.master') @section('title') Welcome @endsection @section('content')
+<div class="container">
+    <div class="row form-sign-signup">
+        <div class="col-md-6">
+			<h3>Sign up</h3>
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="first_name">First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="password">Email</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
 
-		<div class="row">
-				<div class="col-md-6">
-						<form action="" method="POST">
-								<div class="form-group">
-										<label for="email">Email</label>
-										<input class="form-control" type="text" name="email" id="email">
-								</div>
-								<div class="form-group">
-										<label for="first_name">First Name</label>
-										<input class="form-control" type="text" name="first_name" id="first_name">
-								</div>
-								<div class="form-group">
-										<label for="password">Email</label>
-										<input class="form-control" type="password" name="password" id="password">
-								</div>
-								<button type="submit" class="btn btn-primary">Submit</button>
-						</form>
+
+		<div class="col-md-6">
+			<h3>Sign in</h3>
+			<form action="" method="POST">
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input class="form-control" type="text" name="email" id="email">
 				</div>
-			</div>	
-			
+				<div class="form-group">
+					<label for="first_name">First Name</label>
+					<input class="form-control" type="text" name="first_name" id="first_name">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>
+
 	</div>
+
+</div>
 @endsection
